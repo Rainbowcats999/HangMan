ae41360392ab584758f18404a5be0f30b81222c8
Rainbowcats999 <526erizov@frhsd.com>
2025-03-17 16:11:16 +0000

    I'm a bit confused...

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ const keyboard=document.querySelector("keyboard");
 //adding keyboard
 for(let i=97; i<=122;i++){
     const button=document.createElement("button");
-    
+    button.classList.add("btn");
+    button.innerText=String.fromCharCode(i)
+    keyboard.appendChild(button);
 }
