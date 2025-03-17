866861aef972d31d9d5b5ae4b7d6e33e6a6c8b13
Rainbowcats999 <526erizov@frhsd.com>
2025-03-17 16:33:20 +0000

    Fixed the js error

--- wordlist.js
+++ wordlist.js
@@ -8,12 +8,26 @@ const wordList = [
 
     {
     word: "oxygen",
-    hint: A colorless, odorless gas essential for    
-},
+    hint: "A colorless, odorless gas essential for life.",    
+    },
 
     {
     word: "football",
-    hint: A popular sport played with a spherical
+    hint: "A popular sport played with a spherical ball.",
     },
 
+    {
+    word: "history",    
+    hint: "The study of past events and human civilization.",
+    },
+
+    {
+    word: "pizza",
+    hint: "a savory dish consisting of a round, flattened base with toppings.",    
+    },
+
+    {
+    word: "jazz",
+    hint: "A genre of music characterized by improvisation and syncopation.",
+    },
 ]
